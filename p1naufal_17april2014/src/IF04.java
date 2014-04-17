dcb40d3c822a03ddff5c074ed54dc59d9f9a87b4
manbensam <abdulrahmanpancasila@gmail.com>
2014-04-17 16:37:55 +0700

    Pemro01RegulerGenap1314

---
+++ p1naufal_17april2014/src/IF04.java
@@ -0,0 +1,12 @@
+//Created By: Naufal 
+
+package Praktikum05h.bin;
+
+public class IF04
+{
+public static void main (String [] naufal)
+{
+int usia=47;
+System.out.println(usia>50?"anda sudah berumur": "anda masih muda umurnya");
+}
+}
---
+++ p1naufal_17april2014/src/fungsiif.java
@@ -0,0 +1,14 @@
+//Created By: Naufal
+
+package Praktikum05e.bin;
+
+public class fungsiif{
+public static void main (String[] naufal)
+{
+int nilai=68;
+if (nilai >=60)
+{
+System.out.println("Anda LULUS");
+}
+}
+}
